a584bd2fd9e1b1d59745755711386bb8f908625d
rethishchandran <rethishchandran@gmail.com>
2020-05-29 10:30:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 
+<?php 
 function get_random_string($valid_chars, $length)
 {
     $random_string = "";
@@ -36,22 +37,39 @@ function incoming_files() {
   }
   return $files3;
 }
+if(isset($_POST['submit'])){
+    echo " Form Posted <br/>";
+    $tmpFiles = incoming_files();
 
-$tmpFiles = incoming_files();
+    $randomnum = get_random_string("1234567890",5);
 
-$randomnum = get_random_string("1234567890",5);
-
-if($tmpFiles!==""&&count($tmpFiles)>0){
-  echo " Files received count ".count($tmpFiles);
-  foreach ($tmpFiles as $tmpFile ) {
-  echo " Trying to upload File :==> ".basename($tmpFile['name']);
-  $uploaddir = 'genratedfiles/';
-  $tmpfilename= str_replace(' ', '_', basename($tmpFile['name']));
-  $uploadfile = $uploaddir . $subject.$randomnum.$tmpfilename;
-    if (move_uploaded_file($tmpFile['tmp_name'], $uploadfile)) {
-       echo " Uploaded File  :==> ".$uploadfile;
-    } else {
-       echo " Upload failed Filename :==> ".basename($tmpFile['name']);
+    if($tmpFiles!==""&&count($tmpFiles)>0){
+      echo " Files received count ".count($tmpFiles)." <br/>";
+      foreach ($tmpFiles as $tmpFile ) {
+      echo " Trying to upload File :==> ".basename($tmpFile['name'])." <br/>";
+      $uploaddir = 'genratedfiles/';
+      $tmpfilename= str_replace(' ', '_', basename($tmpFile['name']));
+      $uploadfile = $uploaddir . $subject.$randomnum.$tmpfilename;
+        if (move_uploaded_file($tmpFile['tmp_name'], $uploadfile)) {
+           echo " Uploaded File  :==> ".$uploadfile." <br/>";
+        } else {
+           echo " Upload failed Filename :==> ".basename($tmpFile['name'])." <br/>";
+        }
+      }
     }
-  }
 }
+?>
+<html>
+    <head>
+        <title>File Uplod</title>
+    </head>
+    <body>
+        <h1>Submit Form</h1>
+        <form method="POST" action="index.php" enctype="multipart/form-data">
+            <input type="file" name="myUploadFile1" />
+            <input type="file" name="myUploadFile2" />
+            <input type="file" name="myUploadFile3" multiple/>
+            <button type="submit" name="submit" value="submit"/>
+        </form>
+    </body>
+</html>
